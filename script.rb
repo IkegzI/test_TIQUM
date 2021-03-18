8b34c73681e1241c7f0147f557397b71930ea46d
root <root@DESKTOP-179VO8J.localdomain>
2021-03-18 17:48:23 +0800

    хэш с данными по сеансам

--- script.rb
+++ script.rb
@@ -1,2 +1,12 @@
-
+require 'open-uri'
+require 'nokogiri'
 # https://rb7.ru/afisha/movies
+#
+html = open('https://rb7.ru/afisha/movies').read
+doc = Nokogiri::HTML(html)
+hash = {}
+doc.xpath("//*[@class=\"movie\"]").each do |cinema|
+  hash[cinema.css("h2").css("a").text] ||= 0
+  cinema.css('.when').css('.show').each{|ch| hash[cinema.css("h2").css("a").text] += 1}
+end
+
