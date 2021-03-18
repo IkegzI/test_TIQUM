7f0895dc089c787e76740c8da3b111471c576be2
root <root@WIN-SNV3MDU7BI1.localdomain>
2021-03-19 02:33:34 +0800

    исправлен баг на сортировку значений

--- script.rb
+++ script.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
+require 'pry'
 # https://rb7.ru/afisha/movies
 #
 
@@ -22,12 +23,13 @@ doc.xpath("//*[@class=\"movie\"]").each do |cinema|
   hash_session[hash_cinema[cinema.css("h2").css("a").text]] = cinema.css("h2").css("a").text
 end
 
-arr_session_sort_data = hash_cinema.values.sort
+arr_session_sort_data = Hash[hash_cinema.sort_by{|_, value| value}]
 
 1.upto(3) do |i|
-  puts "#{i}. #{hash_session[arr_session_sort_data[i * (-1)]]} (#{arr_session_sort_data[i * (-1)]} пок.)"
+  puts "#{i}. #{arr_session_sort_data.keys[i * (-1)]} (#{arr_session_sort_data[arr_session_sort_data.keys[i * (-1)]]} пок.)"
 end
-
-puts('Нажмите "Enter", чтобы выйти')
+#
+puts
+puts('Нажмите "Enter", чтобы выйти...')
 gets
 clear
